4c86b00f1ae032d611261bc05424877abb1bafed
Ioann <korobkov-1@mail.ru>
2023-02-17 21:20:02 +0300

    number41

---
+++ 41/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+int[] arr = PrintArray();
+int[] PrintArray()
+{
+    int x = Convert.ToInt32(Console.ReadLine());
+    int[] array = new int[x];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-1000, 1000);
+        Console.Write($" {array[i]}");
+    }
+    return array;
+}
+int Zeror(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+Console.WriteLine();
+Console.WriteLine(Zeror(arr));
